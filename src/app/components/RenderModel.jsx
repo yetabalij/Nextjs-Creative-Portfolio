054f9604138609b7e94bbd0e1d588869fc39fd98
ermiastsegu <ermiastsegu@yahoo.com>
2024-03-12 16:33:25 +0300

    render component added.

---
+++ src/app/components/RenderModel.jsx
@@ -0,0 +1,18 @@
+"use client";
+import { Environment } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import clsx from "clsx";
+import React, { Suspense } from "react";
+
+const RenderModel = ({ children, className }) => {
+  return (
+    <div className={clsx("z-10 h-screen w-full relativ", className)}>
+      <Canvas>
+        <Suspense fallback={null}>{children}</Suspense>
+        <Environment preset="dawn" />
+      </Canvas>
+    </div>
+  );
+};
+
+export default RenderModel;
