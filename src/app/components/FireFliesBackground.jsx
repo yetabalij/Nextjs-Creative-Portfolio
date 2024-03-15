e6945c46c5ee8dab0890dc1f7f3367b8502e64f0
ermiastsegu <ermiastsegu@yahoo.com>
2024-03-15 11:27:41 +0300

    firefly completed.

--- src/app/components/FireFliesBackground.jsx
+++ src/app/components/FireFliesBackground.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+const createFirefly = () => ({
+  id: Math.random(),
+  top: `${Math.random() * 100}%`,
+  left: `${Math.random() * 100}%`,
+  animationDuration: `${Math.random() * 5 + 5}s`,
+});
 
 const FireFliesBackground = () => {
-  return <div></div>;
+  const [fireflies, setFireflies] = useState([]);
+
+  useEffect(() => {
+    const addFireflyPeriodicaly = () => {
+      const newFriefly = createFirefly();
+      setFireflies((currentFireflies) => [
+        ...currentFireflies.slice(-14),
+        newFriefly,
+      ]);
+    };
+    const interval = setInterval(addFireflyPeriodicaly, 100);
+    return () => clearInterval(interval);
+  }, [fireflies]);
+
+  return (
+    <div className="fixed top-0 w-full h-full -z-10 overflow-hidden">
+      {fireflies.map((firefly) => {
+        return (
+          <div
+            key={firefly.id}
+            className="absolute rounded-full bg-firefly-radial w-[10px] h-[10px]"
+            style={{
+              top: firefly.top,
+              left: firefly.left,
+              animation: `move ${firefly.animationDuration} infinite alternate`,
+            }}
+          ></div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default FireFliesBackground;
