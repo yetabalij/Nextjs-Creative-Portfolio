6babfc9a57d264b622ad19a18b7b13d8169dc15b
ermiastsegu <ermiastsegu@yahoo.com>
2024-03-26 14:31:22 +0300

    about component added.

--- src/app/(subpages)/about/page.js
+++ src/app/(subpages)/about/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import bg from "../../../../public/background/about-background.png";
+import AboutDetails from "@/app/components/about";
 
 const Home = () => {
   return (
@@ -7,8 +8,15 @@ const Home = () => {
       <Image
         src={bg}
         alt="about-background"
-        className="-z-50 fixed to0` left-0 w-full h-full object-cover object-center opacity-25"
+        className="-z-50 fixed top-0 left-0 w-full h-full object-cover 
+        object-center opacity-25"
       />
+
+      <div className="flex flex-col items-center text-center">
+        <h1 className="font-bold text-9xl text-accent">Hello All,</h1>
+        <p className="text-white">Meet the person behind this portfolio.</p>
+      </div>
+      <AboutDetails />
     </>
   );
 };
